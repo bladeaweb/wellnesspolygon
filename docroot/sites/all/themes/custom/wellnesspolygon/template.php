be3e77db0e288d3d7a109b25b84ff6fb2a8cab3f
Alexander Sharkov <blade.didan@gmail.com>
2017-10-01 19:59:06 +0300

    [#2] Updated styles.

--- docroot/sites/all/themes/custom/wellnesspolygon/template.php
+++ docroot/sites/all/themes/custom/wellnesspolygon/template.php
@@ -4,3 +4,11 @@
  * @file
  * The primary PHP file for this theme.
  */
+
+/**
+ * Implements hook_preprocess_block().
+ */
+function wellnesspolygon_preprocess_block(&$variables) {
+  // Add unique class to each blocks.
+  $variables['classes_array'][] = $variables['block_html_id'];
+}
